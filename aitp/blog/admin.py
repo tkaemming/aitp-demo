1f0fb5aca149cc51afac9db5600c07f0d66a6c6d
Ted Kaemming <ted@kaemming.com>
2010-11-17 10:33:37 -0600

    more advanced post admin

--- aitp/blog/admin.py
+++ aitp/blog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from aitp.blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'posted_at'
+    fieldsets = (
+        ('Post Information', {
+            'fields': ('title', 'author', 'content'),
+        }),
+        ('Advanced Options', {
+            'classes': ('collapse',),
+            'fields': ('slug',)
+        })
+    )
+    list_display = ('title', 'author', 'posted_at')
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+admin.site.register(Post, PostAdmin)
